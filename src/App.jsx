fd391bd791cc46c2e995021615383841de2da20b
jubianiket <jubi.aniket@gmail.com>
2025-06-27 11:50:14 +0530

    updated key

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { RefreshProvider } from './RefreshContext';
 
 const supabase = createClient(
   'https://iwfunipsnoqfasntaofl.supabase.co',
-  'YOUR_PUBLIC_ANON_KEY'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3ZnVuaXBzbm9xZmFzbnRhb2ZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2NzU1MjQsImV4cCI6MjA2NjI1MTUyNH0.E2YU0wDS16TUsIbX8qIM3Xo6XZF3Z_GuWFUmjWw7Z7A'
 );
 
 export default function App() {
